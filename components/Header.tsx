2294067aefc728502b014a74b8b006f43db10f58
MaisonChetty <gamerelites16@gmail.com>
2025-02-26 23:58:40 +0200

    update all

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,15 +9,15 @@ import { Button } from './ui/button'
 function Header() {
   return (
     <header className=' sticky top-0 z-50 left-0 right-0 px-4 md:px-0
-    bg-white/80 backdrop-blur-sm border-b border-gray-200'>
+    bg-black/75 backdrop-blur-md border-b border-black/70'>
         <div className='container mx-auto '>
             <div className=' flex items-center justify-between h-16'>
                 {/**Left */}
                 <div className=' flex items-center justify-between h-16'> 
                     <Link href="/" className='flex items-center gap-4'>
                         {/**AgentPulse */}
-                        <AgentPulse size='small' color='blue'/>
-                        <h1 className='text-xl font-semibold bg-gradient-to-r from-blue-600 to-blue-400 bg-clip-text text-transparent'>
+                        <AgentPulse size='small' color='purple'/>
+                        <h1 className='text-2xl font-semibold bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent'>
                             AgentTube
                         </h1>
                     </Link>
@@ -29,7 +29,7 @@ function Header() {
                         <Link href="/manage-plan">
                         <Button
                             variant="outline"
-                            className='mr-4 bg-gradient-to-r from-blue-600 to-blue-400 text-transparent bg-clip-text'>
+                            className='mr-4 bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text text-xl'>
                                 Manage Plan
                         </Button>
                         </Link>
@@ -44,8 +44,8 @@ function Header() {
                         <Link href="/sign-in">
                             <Button
                                 variant="ghost"
-                                className='bg-gradient-to-r from-blue-600 to-blue-400 
-                                text-transparent bg-clip-text'>
+                                className='bg-gradient-to-r from-blue-400 to-purple-500 
+                                text-transparent bg-clip-text text-xl'>
                                 Sign In
                             </Button>
                         </Link>
