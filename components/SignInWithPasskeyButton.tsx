b0ac235c870998b9f4a9ef16dffa6010ef86cbb8
MaisonChetty <gamerelites16@gmail.com>
2025-02-27 19:05:32 +0200

    day 3

--- components/SignInWithPasskeyButton.tsx
+++ components/SignInWithPasskeyButton.tsx
@@ -1,6 +1,10 @@
 import { useSignIn } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
+function setActive(argo: { session: string | null; }) {
+  throw new Error("Function not implemented.");
+}
+
 export function SignInWithPasskeyButton() {
   const { signIn } = useSignIn();
   const router = useRouter();
@@ -19,5 +23,10 @@ export function SignInWithPasskeyButton() {
       console.error("Error:", JSON.stringify(err, null, 2));
     }
   };
-  return <button onClick={signInWithPasskey}>Sign in with a passkey</button>;
+  return <button onClick={signInWithPasskey} 
+  className="flex w-full items-center justify-center gap-x-3 rounded-md bg-neutral-700 px-3.5 py-1.5 text-sm font-medium text-white shadow-[0_1px_0_0_theme(colors.white/5%)_inset,0_0_0_1px_theme(colors.white/2%)_inset] outline-none hover:bg-gradient-to-b hover:from-white/5 hover:to-white/5 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-white active:bg-gradient-to-b active:from-black/20 active:to-black/20 active:text-white/70"
+  >Sign in with a passkey</button>;
 }
+
+
+
