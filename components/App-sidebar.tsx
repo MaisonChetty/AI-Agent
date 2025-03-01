2f2e718150ca5fb8104b8915571dbf499ca8db47
MaisonChetty <gamerelites16@gmail.com>
2025-03-01 10:23:51 +0200

    sidebar

--- components/App-sidebar.tsx
+++ components/App-sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { Calendar, Home, Inbox,Settings } from "lucide-react";
 
 import {
   Sidebar,
